cc3f0e2a58910e0b4dddc568af4898c4581d47c8
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-02 15:24:17 -0400

    Add Sidebar folder and files
    -Add Sidebar.js file for sidebar component
    -Add basic react component for sidebar
    -Add Sidebar.scss file for styling

---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
+
+import './Sidebar.scss'
+
+class Sidebar extends Component {
+  render () {
+    return (
+      <div className="sidebar">
+        <h5>View All Groups</h5>
+        <h5>Add a Group</h5>
+        <h5>Edit a Group</h5>
+        <h5>Delete a Group</h5>
+      </div>
+    )
+  }
+}
+
+export default Sidebar
