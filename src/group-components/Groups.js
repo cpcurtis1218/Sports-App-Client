18fcd938c44dcffb7238df6ad505ede3ec12666c
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-02 16:05:06 -0400

    Add basic component for Groups
    -Create Groups component
    -Add basic React fragment for now

---
+++ src/group-components/Groups.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Groups extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <p>This is the Groups Component!</p>
+      </React.Fragment>
+    )
+  }
+}
+export default Groups
