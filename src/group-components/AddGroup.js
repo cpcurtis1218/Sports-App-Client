188945d43dc99192d3a3f96d6b4f5caf30d0bf55
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-06 15:17:57 -0400

    Add state, basic form, and input for 'sport'

--- src/group-components/AddGroup.js
+++ src/group-components/AddGroup.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 
 class AddGroup extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      group: {
+        sport: ''
+      }
+    }
+  }
+
   render () {
+    const { sport } = this.state.group
     return (
       <div>
         <h3>Add a new Group</h3>
-        <h4>Sport</h4>
-        <p>Where:</p>
-        <p>When:</p>
+        <form>
+          <h4>Sport:</h4><input required={true} value={sport} type='string' name='sport'/>
+          <p>Where:</p>
+          <p>When:</p>
+        </form>
       </div>
     )
   }
