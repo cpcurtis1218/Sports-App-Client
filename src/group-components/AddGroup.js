d8b168f63f264b5f64dfe338b237e483d36b21aa
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-06 15:22:11 -0400

    Add handleChange event for form updates
    -Changing the for (adding text) will dynamically update the state
    -Add onChange prop to the sport input element

--- src/group-components/AddGroup.js
+++ src/group-components/AddGroup.js
@@ -11,13 +11,19 @@ class AddGroup extends Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({ group: {
+      ...this.state.group, [event.target.name]: event.target.value
+    } })
+  }
+
   render () {
     const { sport } = this.state.group
     return (
       <div>
         <h3>Add a new Group</h3>
         <form>
-          <h4>Sport:</h4><input required={true} value={sport} type='string' name='sport'/>
+          <h4>Sport:</h4><input required={true} value={sport} type='string' name='sport' onChange={this.handleChange}/>
           <p>Where:</p>
           <p>When:</p>
         </form>
