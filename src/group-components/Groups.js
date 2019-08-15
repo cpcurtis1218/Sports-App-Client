d6582cbd25a70abcf8afd590b37cfe325ef55af8
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-15 11:41:38 -0400

    Import Groups.scss file to Groups component

--- src/group-components/Groups.js
+++ src/group-components/Groups.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import apiUrl from '../apiConfig'
 import { Link } from 'react-router-dom'
 
+import './Groups.scss'
+
 class Groups extends Component {
   constructor () {
     super()
