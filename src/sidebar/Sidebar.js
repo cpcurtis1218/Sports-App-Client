c6ef636e1a51757a792446680ae57ffc6e80f066
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-15 11:37:26 -0400

    Add boostrap text centering and remove unused css

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -9,9 +9,9 @@ class Sidebar extends Component {
     return (
       <Container className='sidebar'>
         <Row>
-          <Col><Link to={'/'}>Home</Link></Col>
-          <Col><Link to={'/groups'}>View All Groups</Link></Col>
-          <Col><Link to={'/add-group'}>Add a Group</Link></Col>
+          <Col className='text-center'><Link to={'/'}>Home</Link></Col>
+          <Col className='text-center'><Link to={'/groups'}>View All Groups</Link></Col>
+          <Col className='text-center'><Link to={'/add-group'}>Add a Group</Link></Col>
         </Row>
       </Container>
     )
