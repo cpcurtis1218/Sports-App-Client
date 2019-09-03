4ce17325200f379aad7a941ece28cde8d55bd22a
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-03 14:19:11 -0400

    Update Sidebar to be like Header
    - Passing the user prop to sidebar
    -If user exists, show the Link for Add-Group on the sidebar
    -If the user does not exist, do not show Add-Group Link on sidebar

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 import './Sidebar.scss'
 
-class Sidebar extends Component {
-  render () {
-    return (
-      <Container className='sidebar'>
-        <Row>
-          <Col className='text-center'><Link to={'/'}>Explore</Link></Col>
-          <Col className='text-center'><Link to={'/groups'}>Groups</Link></Col>
-          <Col className='text-center'><Link to={'/add-group'}>New Group</Link></Col>
-        </Row>
-      </Container>
-    )
-  }
-}
+const authenticatedOptions = (
+  <React.Fragment>
+    <Col className='text-center'><Link to={'/'}>Explore</Link></Col>
+    <Col className='text-center'><Link to={'/groups'}>Groups</Link></Col>
+    <Col className='text-center'><Link to={'/add-group'}>New Group</Link></Col>
+  </React.Fragment>
+)
+
+const unauthenticatedOptions = (
+  <React.Fragment>
+    <Col className='text-center'><Link to={'/'}>Explore</Link></Col>
+    <Col className='text-center'><Link to={'/groups'}>Groups</Link></Col>
+    <Col className='text-center'></Col>
+  </React.Fragment>
+)
+
+const Sidebar = ({ user }) => (
+  <Container className='sidebar'>
+    <Row>
+      {user ? authenticatedOptions : unauthenticatedOptions}
+    </Row>
+  </Container>
+)
 
 export default Sidebar
