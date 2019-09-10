1836f6f164abe6610c7b670dc51157c6d6b4312e
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-10 16:36:42 -0400

    Update AddGroup component
    -Add react-bootstrap Button for submit
    -Update title to Create a New Group
    -Update h4's to label elements
    -Add placeholder for city and state inputs
    -Update for more accurate descriptions of inputs

--- src/group-components/AddGroup.js
+++ src/group-components/AddGroup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 import apiUrl from '../apiConfig'
+import Button from 'react-bootstrap/Button'
 
 class AddGroup extends Component {
   constructor (props) {
@@ -61,22 +62,22 @@ class AddGroup extends Component {
     }
     const { sport, city, state, date, time, about } = group
     return (
-      <div>
-        <h2>Add a new Group</h2>
+      <div className="group-form">
+        <h2>Create a New Group</h2>
         <form onSubmit={this.handleSubmit}>
-          <h4>Sport:</h4>
+          <label htmlFor='sport'>Group Name</label>
           <input required={true} value={sport} type='string' name='sport' onChange={this.handleChange}/>
-          <h4>About:</h4>
+          <label htmlFor='about'>Description</label>
           <textarea value={about} type='string' name='about' onChange={this.handleChange}/>
-          <h4>Where:</h4>
-          <input required={true} value={city} type='string' name='city' onChange={this.handleChange}/>
-          <input required={true} value={state} type='string' name='state' onChange={this.handleChange}/>
-          <h4>When:</h4>
+          <label htmlFor='city'>Location</label>
+          <input required={true} value={city} type='string' name='city' placeholder='City' onChange={this.handleChange}/>
+          <input required={true} value={state} type='string' name='state' placeholder='State' onChange={this.handleChange}/>
+          <label htmlFor='date'>Time</label>
           <input required={true} value={date} type='date' name='date' onChange={this.handleChange}/>
-          <p>at</p>
+          <label htmlFor='time'>at</label>
           <input required={true} value={time} type='time' name='time' onChange={this.handleChange}/>
           <br/>
-          <button type='submit'>Submit</button>
+          <Button className="secondary" type='submit'>Submit</Button>
         </form>
       </div>
     )
