1f5f5d4897aa0f061e5f97cf6a39a721a69b1851
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-13 12:50:08 -0400

    Add My Groups list to profile
    -Add state to component with a groups array
    -Add componentDidMount to get groups list
    -Add groups array filter to show current user's groups
    -Add basic list to display list of user's groups
    -Use same styling as the GroupsList component

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+import apiUrl from '../apiConfig'
+import { Container, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
-const Profile = ({ user }) => (
-  <React.Fragment>
-    <p>This is the Profile</p>
-  </React.Fragment>
-)
+class Profile extends Component {
+  constructor (props) {
+    super()
+
+    this.state = {
+      groups: []
+    }
+  }
+
+  componentDidMount (props) {
+    axios({
+      url: `${apiUrl}/groups`,
+      method: 'get'
+    })
+      .then(response => this.setState({
+        groups: response.data.groups
+      }))
+      .catch(() => console.log('Something went wrong.'))
+  }
+
+  render () {
+    const { groups } = this.state
+    if (!groups.length) {
+      return (
+        <p>You haven&apos;t created any groups!  Click New Group to change that!</p>
+      )
+    } else {
+      const myGroups = groups.filter(group => group.user_id === this.props.user.id)
+      return (
+        <React.Fragment>
+          <Container className='groups-list mt-2'>
+            <h2>My Groups:</h2>
+            {myGroups.map(group => (
+              <Row key={group.id} className='group-list-item'>
+                <Col xs={8} className='p-2'>
+                  <h3><Link to={{
+                    pathname: '/groups/' + group.id
+                  }}>{group.sport.length > 12 ? group.sport.substring(0, 12) + '...' : group.sport}</Link></h3>
+                </Col>
+              </Row>
+            ))}
+          </Container>
+        </React.Fragment>
+      )
+    }
+  }
+}
 
 export default Profile
