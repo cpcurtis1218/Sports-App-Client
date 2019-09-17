69d879c1c408737be7906692816e8d34bf2c47f8
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-17 16:27:58 -0400

    Sidebar.js Updates
    -Remove text-center from Col
    -Add p-0 to Col so that <a> takes up the entire div

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -6,17 +6,17 @@ import './Sidebar.scss'
 
 const authenticatedOptions = (
   <React.Fragment>
-    <Col className='text-center'><Link to={'/'}>Explore</Link></Col>
-    <Col className='text-center'><Link to={'/groups'}>Groups</Link></Col>
-    <Col className='text-center'><Link to={'/add-group'}>New Group</Link></Col>
+    <Col className='p-0'><Link to={'/'}>Explore</Link></Col>
+    <Col className='p-0'><Link to={'/groups'}>Groups</Link></Col>
+    <Col className='p-0'><Link to={'/add-group'}>New Group</Link></Col>
   </React.Fragment>
 )
 
 const unauthenticatedOptions = (
   <React.Fragment>
-    <Col className='text-center'><Link to={'/'}>Explore</Link></Col>
-    <Col className='text-center'><Link to={'/groups'}>Groups</Link></Col>
-    <Col className='text-center'></Col>
+    <Col className='p-0'><Link to={'/'}>Explore</Link></Col>
+    <Col className='p-0'><Link to={'/groups'}>Groups</Link></Col>
+    <Col className='p-0'></Col>
   </React.Fragment>
 )
 
