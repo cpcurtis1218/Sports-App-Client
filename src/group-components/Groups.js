892a897f147a0eb6ab1fd5913690b0832a63a2dc
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-24 10:39:21 -0400

    Groups.js - Move dateFormat and timeFormat
    -Remove dateFormat and timeFormat from this file
    -Import Methods.js file
    -Add Methods.dateFormat for date display
    -Add Methods.timeFormat for time display

--- src/group-components/Groups.js
+++ src/group-components/Groups.js
@@ -4,6 +4,7 @@ import apiUrl from '../apiConfig'
 import { Link } from 'react-router-dom'
 import PeopleIcon from '../assets/people-icon.png'
 import { Container, Row, Col } from 'react-bootstrap'
+import Methods from '../assets/Methods.js'
 
 import './Groups.scss'
 
@@ -27,25 +28,6 @@ class Groups extends Component {
       .catch(() => console.log('Something went wrong.'))
   }
 
-  dateFormat (date) {
-    const dateArr = date.split('-')
-    return `${dateArr[1]}/${dateArr[2]}/${dateArr[0][2]}${dateArr[0][3]}`
-  }
-
-  timeFormat (time) {
-    const timeArr = time.split(':')
-    // if the hours are less than 10 (single digit), use the digit and minutes
-    if (timeArr[0] < 10) {
-      return timeArr[0][1] + ':' + timeArr[1] + 'AM'
-    // if the hours are less than 12, just use the time + AM
-    } else if (timeArr[0] < 12) {
-      return time + 'AM'
-    // else just subtract 12 from the hours and add PM
-    } else {
-      return timeArr[0] - 12 + ':' + timeArr[1] + 'PM'
-    }
-  }
-
   render () {
     const { groups } = this.state
     if (!groups.length) {
@@ -61,8 +43,8 @@ class Groups extends Component {
             {groups.map(group => (
               <Row key={group.id} className='group-list-item'>
                 <Col xs={2} className='p-2'>
-                  <p>{this.timeFormat(group.time)}</p>
-                  <p>{this.dateFormat(group.date)}</p>
+                  <p>{Methods.timeFormat(group.time)}</p>
+                  <p>{Methods.dateFormat(group.date)}</p>
                 </Col>
                 <Col xs={8} className='p-2'>
                   <h3><Link to={{
