30c61cbaa5b5ec61796136ad6d7494dd4f5af894
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-24 10:04:41 -0400

    Groups.js - Refactor timeFormat and dateFormat
    -In dateFormat, use dateArr instead of calling split multiple times
    -In timeFormat, take out last if/else statement, just use final else

--- src/group-components/Groups.js
+++ src/group-components/Groups.js
@@ -28,17 +28,19 @@ class Groups extends Component {
   }
 
   dateFormat (date) {
-    return `${date.split('-')[1]}/${date.split('-')[2]}/${date.split('-')[0][2]}${date.split('-')[0][3]}`
+    const dateArr = date.split('-')
+    return `${dateArr[1]}/${dateArr[2]}/${dateArr[0][2]}${dateArr[0][3]}`
   }
 
   timeFormat (time) {
     const timeArr = time.split(':')
+    // if the hours are less than 10 (single digit), use the digit and minutes
     if (timeArr[0] < 10) {
       return timeArr[0][1] + ':' + timeArr[1] + 'AM'
+    // if the hours are less than 12, just use the time + AM
     } else if (timeArr[0] < 12) {
       return time + 'AM'
-    } else if (timeArr[0] < 22) {
-      return timeArr[0] - 12 + ':' + timeArr[1] + 'PM'
+    // else just subtract 12 from the hours and add PM
     } else {
       return timeArr[0] - 12 + ':' + timeArr[1] + 'PM'
     }
