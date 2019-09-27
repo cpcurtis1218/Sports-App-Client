f3e01e00d1fc3a2a347f73ff9f62bdd2202102d1
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-27 15:02:14 -0400

    Explore.js - Add Google Map Component
    -Import GoogleMapReact from package
    -Add div for map component
    -Add default zoom and default center to map

--- src/explore/Explore.js
+++ src/explore/Explore.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
 
 const Explore = ({ user }) => (
-  <h1>This is the Explore Page!</h1>
+  <div style={{ 'height': '500px', 'width': '500px', 'margin-left': '200px' }}>
+    <GoogleMapReact
+      defaultCenter={{ lat: 42.3512354, lng: -71.0584297 }}
+      defaultZoom={12}
+    >
+    </GoogleMapReact>
+  </div>
 )
 
 export default Explore
