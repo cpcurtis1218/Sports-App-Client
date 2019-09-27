e6af130e7d9eec7f63e31764ac29911cc162c1bf
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-27 15:59:51 -0400

    Explore.js - Add access to GOOGLE_MAP_API_KEY

--- src/explore/Explore.js
+++ src/explore/Explore.js
@@ -2,8 +2,9 @@ import React from 'react'
 import GoogleMapReact from 'google-map-react'
 
 const Explore = ({ user }) => (
-  <div style={{ 'height': '500px', 'width': '500px', 'margin-left': '200px' }}>
+  <div style={{ 'height': '500px', 'width': '500px', 'marginLeft': '250px' }}>
     <GoogleMapReact
+      bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API_KEY }}
       defaultCenter={{ lat: 42.3512354, lng: -71.0584297 }}
       defaultZoom={12}
     >
