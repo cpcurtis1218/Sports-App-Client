2efa430ca8a03223607c03c6c8087eafdb3fb158
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-07 16:12:50 -0400

    Remove secret key from Google Maps Component
    -Display is now the developer version
    -Not adding Billing to Google cloud services at this time.

--- src/explore/Explore.js
+++ src/explore/Explore.js
@@ -4,7 +4,6 @@ import GoogleMapReact from 'google-map-react'
 const Explore = ({ user }) => (
   <div style={{ 'height': '500px', 'width': '500px', 'marginLeft': '250px' }}>
     <GoogleMapReact
-      bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API_KEY }}
       defaultCenter={{ lat: 42.3512354, lng: -71.0584297 }}
       defaultZoom={12}
     >
