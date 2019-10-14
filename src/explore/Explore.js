4f245ed90e0b63c67453a29f11c4ff9328dcf36d
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-14 16:11:24 -0400

    Explore.js
    -Add div for explaining the app

--- src/explore/Explore.js
+++ src/explore/Explore.js
@@ -2,12 +2,21 @@ import React from 'react'
 import GoogleMapReact from 'google-map-react'
 
 const Explore = ({ user }) => (
-  <div style={{ 'height': '500px', 'width': '500px', 'marginLeft': '250px' }}>
-    <GoogleMapReact
-      defaultCenter={{ lat: 42.3512354, lng: -71.0584297 }}
-      defaultZoom={12}
-    >
-    </GoogleMapReact>
+  <div>
+    <div style={{ 'color': 'white', 'width': '500px', 'marginLeft': '250px' }}>
+      Welcome to Let&apos;s Go{user ? ', ' + user.email : ''}!
+      <br/>
+      <br/>
+      Find a group nearby and join in the fun, or create you&apos;re own group
+      and invite others to join you!
+    </div>
+    <div style={{ 'height': '500px', 'width': '500px', 'marginLeft': '250px', 'marginTop': '2rem' }}>
+      <GoogleMapReact
+        defaultCenter={{ lat: 42.3512354, lng: -71.0584297 }}
+        defaultZoom={12}
+      >
+      </GoogleMapReact>
+    </div>
   </div>
 )
 
