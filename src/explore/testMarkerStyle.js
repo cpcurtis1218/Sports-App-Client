97e363a0bde6421f67d2c4bee0add921535519a4
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-17 18:21:35 -0400

    Add testMarkerStyle.js file
    -Styling for map markers

---
+++ src/explore/testMarkerStyle.js
@@ -0,0 +1,18 @@
+const testMarkerStyle = {
+  position: 'absolute',
+  width: 40,
+  height: 30,
+  left: -20,
+  top: -15,
+
+  border: '2px solid green',
+  borderRadius: 5,
+  backgroundColor: 'white',
+  textAlign: 'center',
+  color: 'black',
+  fontSize: 8,
+  fontWeight: 'bold',
+  padding: 2
+}
+
+export { testMarkerStyle }
