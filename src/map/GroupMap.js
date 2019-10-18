f5ce55b1e23d0207476a1706a07e08535eaa22fc
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-18 14:32:01 -0400

    GroupMap.js
    -Add import Marker file
    -Render Marker component within Map component

--- src/map/GroupMap.js
+++ src/map/GroupMap.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
+import Marker from './Marker.js'
 
-const GroupMap = (props, markers) => (
+const GroupMap = (props) => (
   <GoogleMapReact
     // bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
     defaultCenter={props.defaultCenter}
     defaultZoom={props.defaultZoom}
   >
+    <Marker
+      text='Test Marker'
+      lat={42.325}
+      lng={-71.05}
+    />
   </GoogleMapReact>
 )
 
