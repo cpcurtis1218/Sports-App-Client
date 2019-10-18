83450b9a842e890afe28617348cabff116d2fa64
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-18 14:33:20 -0400

    Add markerStyle file

---
+++ src/map/markerStyle.js
@@ -0,0 +1,18 @@
+const markerStyle = {
+  position: 'absolute',
+  width: 40,
+  height: 30,
+  left: -20,
+  top: -15,
+
+  border: '2px solid green',
+  borderRadius: 5,
+  backgroundColor: 'white',
+  textAlign: 'center',
+  color: 'black',
+  fontSize: 8,
+  fontWeight: 'bold',
+  padding: 2
+}
+
+export { markerStyle }
