7290fb51e1206be11a786f4294ea5fbcebdf3e8f
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-18 15:37:19 -0400

    GroupMap.js - Marker Updates
    -Update a single Marker to using an array of Markers to display
      on the Map

--- src/map/GroupMap.js
+++ src/map/GroupMap.js
@@ -2,17 +2,26 @@ import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import Marker from './Marker.js'
 
+const markerList = [
+  {
+    text: 'Test Marker',
+    lat: 42.325,
+    lng: -71.05
+  },
+  {
+    text: 'Test Marker 2',
+    lat: 42.38,
+    lng: -71.06
+  }
+]
+
 const GroupMap = (props) => (
   <GoogleMapReact
     // bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
     defaultCenter={props.defaultCenter}
     defaultZoom={props.defaultZoom}
   >
-    <Marker
-      text='Test Marker'
-      lat={42.325}
-      lng={-71.05}
-    />
+    {markerList.map(o => <Marker key={o.text} text={o.text} lat={o.lat} lng={o.lng}/>)}
   </GoogleMapReact>
 )
 
