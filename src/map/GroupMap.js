0ff3efd6d0bc897493a92d3fc74c0cf01c33a093
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-23 15:26:40 -0400

    Update GroupMap to React Component

--- src/map/GroupMap.js
+++ src/map/GroupMap.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 import Marker from './Marker.js'
 
@@ -20,14 +20,27 @@ const markerList = [
   }
 ]
 
-const GroupMap = (props) => (
-  <GoogleMapReact
-    bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
-    defaultCenter={props.defaultCenter}
-    defaultZoom={props.defaultZoom}
-  >
-    {markerList.map((m, i) => <Marker key={i} text={m.text} lat={m.lat} lng={m.lng}/>)}
-  </GoogleMapReact>
-)
+class GroupMap extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      defaultCenter: props.defaultCenter,
+      defaultZoom: props.defaultZoom
+    }
+  }
+
+  render () {
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={this.state.defaultCenter}
+        defaultZoom={this.state.defaultZoom}
+      >
+        {markerList.map((m, i) => <Marker key={i} text={m.text} lat={m.lat} lng={m.lng}/>)}
+      </GoogleMapReact>
+    )
+  }
+}
 
 export default GroupMap
